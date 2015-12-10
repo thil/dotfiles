35c0d375d85bb63a1156c2772d995ecbe7dc4130
Thil Bandara <tbandara@gmail.com>
2015-12-10 11:51:19 +1100

    Update vagrant bootstrap

--- home/vagrant/bootstrap.sh
+++ home/vagrant/bootstrap.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y git
+apt-get install -y git zsh
+
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9/
+sudo make install
+
+cd $HOME
+
+wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
+tar -xzvf ruby-install-0.5.0.tar.gz
+cd ruby-install-0.5.0/
+sudo make install
