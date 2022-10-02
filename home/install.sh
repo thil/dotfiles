7ad653f0e2c40d86a06d1ee5975ed6f8168d27ab
T <tbandara@gmail.com>
2022-10-02 20:05:04 +1100

    Install Prezto

--- home/install.sh
+++ home/install.sh
@@ -1,23 +1,24 @@
 #!/bin/bash -e
 
 function brewInstall {
-	if command -v brew; then
-		echo "Installed brew skipping"
-	else
-		echo "======================================================"
-		echo "Installing Homebrew, the OSX package manager...If it's"
-		echo "already installed, this will do nothing."
-		echo "======================================================"
-		echo ""
+  if command -v brew; then
+    echo "Installed brew skipping"
+  else
+    echo "======================================================"
+    echo "Installing Homebrew, the OSX package manager...If it's"
+    echo "already installed, this will do nothing."
+    echo "======================================================"
+    echo ""
+    
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/thil/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
-	fi
+  fi
 }
 
 function brewRun {
-	brew bundle
-	set -U fish_user_paths /opt/homebrew/bin/ $fish_user_path
+  brew bundle
+  set -U fish_user_paths /opt/homebrew/bin/ $fish_user_path
 }
 
 function plugInstall {
@@ -35,29 +36,16 @@ function plugRun {
   vim --noplugin -u .vim/bundle.vim -N \"+set hidden\" \"+syntax on\" +PlugClean +PlugInstall +qall
 }
 
-function installFisher {
-  fish
-  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+function installPrezto {
+  zsh
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
 }
 
-function installFisherPlugins {
-  fish
-  fisher install pure-fish/pure
-  fisher install jethrokuan/z
-  fisher install PatrickF1/fzf.fish
-  echo -e '\nsource "(brew --prefix asdf)"/asdf.fish' >> ~/.config/fish/config.fish
-}
-
-function setUpAliases {
-  fish
-  alias vi 'nvim'
-  alias vim 'nvim'
-  alias ls 'exa'
-
-  funcsave vi
-  funcsave vim
-  funcsave exa
-}
 
 function fasterScrollSublimeText {
   defaults write com.sublimetext.4 ApplePressAndHoldEnabled -bool false
@@ -69,9 +57,6 @@ brewRun
 plugInstall
 PlugRun
 
-installFisher
-installFisherPlugins
-
-setupAliases
+installPrezto
 
 fasterScrollSbulimeText
