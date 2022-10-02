be2d9ef6a83223faa8e7ca80ce83551d2ef630ee
T <tbandara@gmail.com>
2022-10-02 20:05:29 +1100

    Set zsh as default shell

--- home/install.sh
+++ home/install.sh
@@ -44,6 +44,8 @@ function installPrezto {
   for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
   done
+  
+  chsh -s /bin/zsh
 }
 
 
